38e92629b7ac764e90337f79529be6ec0f689fd1
Korra <108448704+Korra1234@users.noreply.github.com>
2024-05-21 18:20:56 +0400

    Update ChangeTest.java

--- ChangeTest.java
+++ ChangeTest.java
@@ -1,4 +1,4 @@
-/Author: Siyeshen Milen Govender
+/Author: Korra
 Purpose:User enter person name and change(how many times they want to), a Menu is displayed and they select an option for their desired calculation
  
 Assumptions: The user understands English
